95073d5ac1b8e7662d5e986d090ce0108fb397d7
gandeful <863529565@qq.com>
2017-12-23 00:40:35 +0800

    jq优化,版本2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 
 
-function ajax(url,callback){
-    var api ="http://10.18.2.131:3000";
+function ajax(url, callback) {
+    var api = "http://10.18.2.131:3000";
     $.ajax({
-        url : api+url,
+        url: api + url,
         success: callback
     })
 }
 
 
 //请求header的数据
-ajax('/header',function(data){
+ajax('/header', function (data) {
     console.log(data);
     var html = "<ul>";
-    for(var i = 0 ; i < data.length ; i++){
-        var li = ['<li><a href="#">',data[i],'</a></li>'];
-        html+= li.join('');//分隔符为空字符串,将数组转化为字符串
+    for (var i = 0; i < data.length; i++) {
+        var li = ['<li><a href="#">', data[i], '</a></li>'];
+        html += li.join('');//分隔符为空字符串,将数组转化为字符串
     }
     html += "</ul>";
-    //拓展,jq设置css
 
+    /* 
+    //拓展,jq设置css
     $('#header').html(html);//渲染标签
     //因为ul里面的li是浮动元素,所以,没有高度,所以对 ul标签清除浮动
     
@@ -30,7 +31,20 @@ ajax('/header',function(data){
         "listStyle":"none",
         "marginLeft":"5px"
     });
-    
+    */
+
+    //jq优化,find查找子元素
+    $('#header')
+        .html(html)//渲染标签
+        .find('ul')//找到ul清除浮动
+        .css('overflow', 'hidden')
+        .find('li')//设置li浮动
+        .css({
+            "float": "left",
+            "listStyle": "none",
+            "marginLeft": "5px"
+        })
+
 })
 
 
