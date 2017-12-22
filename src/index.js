7995be032c9fb2c39f65b0318149682ee64e5d01
gandeful <863529565@qq.com>
2017-12-23 00:48:17 +0800

    抽取公共部分,版本3

--- src/index.js
+++ src/index.js
@@ -8,16 +8,46 @@ function ajax(url, callback) {
     })
 }
 
+function map(startTag) {
+    // <ul> => </u l > 
+    var arr = startTag.split('');//切割为数组
+    arr[0]+= '/';//添加/
+    return arr.join('');//转换为字符串
+}
+
+function fillOperate(id, data, startTag,itemTag) {
+    var html = startTag;
+    for (var i = 0; i < data.length; i++) {
+        var li = [itemTag, data[i], map(itemTag)];
+        html += li.join('');//分隔符为空字符串,将数组转化为字符串
+    }
+    html += map(startTag);
+
+    $('#header')
+        .html(html)//渲染标签
+        .find('ul')//找到ul清除浮动
+        .css('overflow', 'hidden')
+        .find('li')//设置li浮动
+        .css({
+            "float": "left",
+            "listStyle": "none",
+            "marginLeft": "5px"
+        })
+}
+
 
 //请求header的数据
 ajax('/header', function (data) {
-    console.log(data);
+
+    fillOperate('#header', data,'<ul>','<li>');
+    /*
     var html = "<ul>";
     for (var i = 0; i < data.length; i++) {
         var li = ['<li><a href="#">', data[i], '</a></li>'];
         html += li.join('');//分隔符为空字符串,将数组转化为字符串
     }
     html += "</ul>";
+    */
 
     /* 
     //拓展,jq设置css
@@ -34,7 +64,7 @@ ajax('/header', function (data) {
     */
 
     //jq优化,find查找子元素
-    $('#header')
+    /*$('#header')
         .html(html)//渲染标签
         .find('ul')//找到ul清除浮动
         .css('overflow', 'hidden')
@@ -43,7 +73,7 @@ ajax('/header', function (data) {
             "float": "left",
             "listStyle": "none",
             "marginLeft": "5px"
-        })
+        })*/
 
 })
 
